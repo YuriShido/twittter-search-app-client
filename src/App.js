7f0984b635698abf1fc6cbda512b666c7eae53d7
YuriShido <sydonce57@gmail.com>
2021-08-17 13:38:22 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import './App.css';
+import axios from 'axios'
+// import socketIo from  'socket.io'
+import TweetCard from './component/TweetCard'
+
+
+function App() {
+  const [serverData, setServerData] = useState ({ search_metadata: {}, statuses: []})
+  const [input, setInput] = useState()
+  const [sendInput, setSendInput] = useState()
+  const [toggle, setToggle] = useState(true)
+
+  useEffect(() => {
+    const getData = () => {
+      
+      axios.get("http://localhost:8000/")
+      .then((response) => {
+        // console.log('data!!!', response.data.statuses[0].created_at);
+        console.log(response.data);
+        setServerData(response.data)
+
+      })
+      .then(setToggle(true))
+      .catch( err => console.log(err))
+      
+    }
+    
+    getData()
+    
+  }, [input,toggle])
+  console.log('data:',serverData);
+  
+  const handleSearch = async(e) => {
+    e.preventDefault()
+    // setSendInput(input)
+    // console.log(sendInput);
+    try {
+      const inputData = {
+        input
+      }
+      console.log(input);
+      await axios.post("http://localhost:8000/search", inputData)
+      .then(setToggle(false))
+      console.log('toggle:',toggle);
+    } catch (error) {
+      console.log("Error: ",error.response)
+
+    }
+  }
+  const handleClick = async() => {
+      await axios.get("http://localhost:8000/update")
+      .then((response) => {
+        // console.log('data!!!', response.data.statuses[0].created_at);
+        setServerData(response.data)
+      })
+  }
+  // const handleOnChange = (e) => {
+  //     setInput(e.target.value)
+  //     console.log(input);
+  //     setSendInput(input)
+  // }
+
+  return (
+    <div className="App">
+      {/* <div className="header-container"> */}
+
+      <div className="header">
+      <h1 className="title">Search the Tweet</h1>
+      <form onSubmit={handleSearch}>
+        <input className="input" type="text" onChange={(e) => setInput(e.target.value)}/>
+        {/* <input type="text" onChange={(e) => handleOnChange(e)}/> */}
+        <input className="search-btn" type="submit" value="search"/>
+      </form>
+      <button className="btn" onClick={handleClick}>Web Job</button>
+      </div>
+      {/* </div> */}
+      {/* {
+        serverData ?
+        (serverData.statuses.map(tweet => (
+          <div key={tweet.id}>
+          <p>{tweet.created_at}</p>
+          <p>{tweet.text}</p>
+          </div>
+        ))
+      ) : null
+      } */}
+      <div>
+      <TweetCard serverData={serverData} />
+      
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/TweetCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../App.css';
+
+const TweetCard = ({ serverData }) => {
+    return (
+        <div className="TweetCard-container">
+            {
+                serverData && serverData.statuses.map(tweet => (
+
+                    <div key={tweet.id} className="card clearfix">
+                        <div className='user'>
+                            <img className="icon-img" src={tweet.user.profile_image_url} />
+                            <p>{tweet.user.name}</p>
+                            <p className='date'>{new Date(tweet.created_at).toLocaleTimeString()}</p>
+                            {/* {new Date(tweet.created_at).toLocaleDateString()} */}
+                        </div>
+                        {/* <p>{tweet.retweeted_status.text}</p> */}
+                        <p className="text">{tweet.text}</p>
+                        <a className="link" href={`https://twitter.com/${tweet.user.screen_name}`}>Go to page <i className="fab fa-twitter"></i></a>
+                        {/* /status/${tweet.id} */}
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default TweetCard
